ae96fbce8ffe512220188a535a2860a7179960b6
YRELIS <y.elis@mail.ru>
2017-11-25 20:30:42 +0300

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FileExplorer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._backButton = new System.Windows.Forms.Button();
+            this._forwardButton = new System.Windows.Forms.Button();
+            this._pathText = new System.Windows.Forms.TextBox();
+            this._goButton = new System.Windows.Forms.Button();
+            this._listView = new System.Windows.Forms.ListView();
+            this.file = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.type = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this._treeView = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // _backButton
+            // 
+            this._backButton.Location = new System.Drawing.Point(13, 12);
+            this._backButton.Name = "_backButton";
+            this._backButton.Size = new System.Drawing.Size(27, 23);
+            this._backButton.TabIndex = 0;
+            this._backButton.Text = "<-";
+            this._backButton.UseVisualStyleBackColor = true;
+            // 
+            // _forwardButton
+            // 
+            this._forwardButton.Location = new System.Drawing.Point(46, 12);
+            this._forwardButton.Name = "_forwardButton";
+            this._forwardButton.Size = new System.Drawing.Size(27, 23);
+            this._forwardButton.TabIndex = 0;
+            this._forwardButton.Text = "->";
+            this._forwardButton.UseVisualStyleBackColor = true;
+            // 
+            // _pathText
+            // 
+            this._pathText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._pathText.Location = new System.Drawing.Point(80, 12);
+            this._pathText.Name = "_pathText";
+            this._pathText.Size = new System.Drawing.Size(582, 20);
+            this._pathText.TabIndex = 1;
+            this._pathText.Text = "Мой компьютер";
+            // 
+            // _goButton
+            // 
+            this._goButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this._goButton.Location = new System.Drawing.Point(668, 12);
+            this._goButton.Name = "_goButton";
+            this._goButton.Size = new System.Drawing.Size(30, 23);
+            this._goButton.TabIndex = 2;
+            this._goButton.Text = "Go";
+            this._goButton.UseVisualStyleBackColor = true;
+            this._goButton.Click += new System.EventHandler(this._goButton_Click_1);
+            // 
+            // _listView
+            // 
+            this._listView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.file,
+            this.size,
+            this.type});
+            this._listView.Location = new System.Drawing.Point(199, 58);
+            this._listView.Name = "_listView";
+            this._listView.Size = new System.Drawing.Size(499, 363);
+            this._listView.TabIndex = 3;
+            this._listView.UseCompatibleStateImageBehavior = false;
+            this._listView.View = System.Windows.Forms.View.Details;
+            this._listView.SelectedIndexChanged += new System.EventHandler(this._listView_SelectedIndexChanged);
+            // 
+            // file
+            // 
+            this.file.Text = "Файл";
+            this.file.Width = 183;
+            // 
+            // size
+            // 
+            this.size.Text = "Размер (МБ)";
+            this.size.Width = 109;
+            // 
+            // type
+            // 
+            this.type.Text = "Тип";
+            this.type.Width = 98;
+            // 
+            // _treeView
+            // 
+            this._treeView.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this._treeView.Location = new System.Drawing.Point(13, 58);
+            this._treeView.Name = "_treeView";
+            this._treeView.Size = new System.Drawing.Size(180, 363);
+            this._treeView.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Каталоги";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(199, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Файлы";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(710, 433);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._treeView);
+            this.Controls.Add(this._listView);
+            this.Controls.Add(this._goButton);
+            this.Controls.Add(this._pathText);
+            this.Controls.Add(this._forwardButton);
+            this.Controls.Add(this._backButton);
+            this.Name = "MainForm";
+            this.Text = "Файловый менеджер";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _backButton;
+        private System.Windows.Forms.Button _forwardButton;
+        private System.Windows.Forms.TextBox _pathText;
+        private System.Windows.Forms.Button _goButton;
+        private System.Windows.Forms.ListView _listView;
+        private System.Windows.Forms.TreeView _treeView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ColumnHeader file;
+        private System.Windows.Forms.ColumnHeader size;
+        private System.Windows.Forms.ColumnHeader type;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing;
+
+namespace FileExplorer
+{
+    public partial class MainForm : Form
+    {
+
+        List<string> _pathMove = new List<string>();
+        int _moveIndex = -1;
+        string _currentPath = "";
+        ImageList _imageList = new ImageList();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Load += MainForm_Load;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            loadBaseTree();
+
+            _treeView.NodeMouseDoubleClick += _treeView_NodeMouseDoubleClick;
+            _listView.DoubleClick += _listView_DoubleClick;
+
+            _pathText.KeyUp += _pathText_KeyUp;
+            _goButton.Click += _goButton_Click;
+
+            _backButton.Click += _backButton_Click;
+            _forwardButton.Click += _forwardButton_Click;
+
+            _imageList.Images.Add(Image.FromFile("folder.ico"));
+            _imageList.Images.Add(Image.FromFile("disk.png"));
+
+            _treeView.ImageList = _imageList;
+        }
+
+        private void _goButton_Click(object sender, EventArgs e)
+        {
+            goPath(_pathText.Text, true);
+        }
+
+        private void _forwardButton_Click(object sender, EventArgs e)
+        {
+            if (_moveIndex == -1 || _pathMove.Count == _moveIndex + 1) return;
+            goPath(_pathMove[++_moveIndex]);
+        }
+
+        private void _backButton_Click(object sender, EventArgs e)
+        {
+            if (_moveIndex == -1 || _moveIndex - 1 == -1) return;
+            goPath(_pathMove[--_moveIndex]);
+        }
+
+        private void _pathText_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Enter)
+            {
+                goPath(_pathText.Text, true);
+            }
+        }
+
+        void goPath(string path, bool savePath = false)
+        {
+            try
+            {
+                string[] sPath = path.Split('\\');
+                string drive = sPath[0] + "\\";
+
+                TreeNode tn = null;
+                foreach(TreeNode t in _treeView.Nodes)
+                {
+                    if(t.Text == drive)
+                    {
+                        tn = t;
+                        break;
+                    }
+                }
+
+                loadTreeNode(tn);
+                for (int i = 1; i < sPath.Length; i++)
+                {
+                    foreach (TreeNode t in tn.Nodes)
+                    {
+                        if (t.Text == sPath[i])
+                        {
+                            tn = t;
+                            break;
+                        }
+                    }
+                    loadTreeNode(tn);
+                    if (tn == null) throw new Exception();
+                }
+                if (savePath)
+                {
+                    _pathMove.RemoveRange(_moveIndex + 1, _pathMove.Count - _moveIndex - 1);
+                    _pathMove.Add(path);
+                    _moveIndex = _pathMove.Count - 1;
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Неверный путь");
+            }
+
+        }
+
+        private void _listView_DoubleClick(object sender, EventArgs e)
+        {
+            ListView.SelectedListViewItemCollection ic = _listView.SelectedItems;
+            if(ic.Count > 0)
+            {
+                try
+                {
+                    ListViewItem li = ic[0];
+                    string type = li.SubItems[2].Text;
+                    string fileName = li.Text;
+                    string fullFileName = _currentPath + (_currentPath.LastIndexOf('\\') == _currentPath.Length - 1 ? "" : "\\") + fileName + (type == "" ? "" : "." + type);
+                    System.Diagnostics.Process.Start(fullFileName);
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+        }
+
+        void loadTreeNode(TreeNode tn, bool savePath = false)
+        {
+            if (tn != null)
+            {
+                string path = tn.Text;
+                TreeNode parent = tn.Parent;
+                while (parent != null)
+                {
+                    path = path.Insert(0, parent.Text + (parent.Text.IndexOf('\\') == -1 ? "\\" : ""));
+                    parent = parent.Parent;
+                }
+                try
+                {
+                    setFilesToListFromDir(path);
+                    string[] dirs = getDirsInDir(path);
+                    if (tn.Nodes.Count == 0)
+                    {
+                        foreach (string dir in dirs)
+                        {
+                            string[] splited = dir.Split('\\');
+                            string dirName = splited[splited.Length - 1];
+                            tn.Nodes.Add(dirName).ImageIndex = 0;
+                        }
+                    }
+                    _currentPath = path;
+                    _pathText.Text = _currentPath;
+                    if (savePath) {
+                        _pathMove.RemoveRange(_moveIndex + 1, _pathMove.Count - _moveIndex - 1);
+                        _pathMove.Add(_currentPath);
+                        _moveIndex = _pathMove.Count - 1;
+                    }
+                    if (_pathMove.Count > 20) _pathMove.RemoveAt(0);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    MessageBox.Show("Требуются права администратора");
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+                tn.Expand();
+            }
+        }
+
+        private void _treeView_NodeMouseDoubleClick(object sender, TreeNodeMouseClickEventArgs e)
+        {
+            loadTreeNode(_treeView.SelectedNode, true);
+        }
+
+        void setFilesToListFromDir(string dir)
+        {
+            _listView.Items.Clear();
+            string[] files = Directory.GetFiles(dir);
+            foreach(string file in files)
+            {
+                FileInfo fi = new FileInfo(file);
+                string[] name = fi.Name.Split('.');
+                for(int i = 1; i < name.Length - 1; i++)
+                {
+                    name[0] += "." + name[i];
+                }
+                string[] row = { fi.Length.ToString(), (name.Length > 1 ? name[name.Length - 1] : "") };
+                _listView.Items.Add(name[0]).SubItems.AddRange(row);
+            }
+        }
+
+        string [] getLogicalDrives()
+        {
+            return Directory.GetLogicalDrives();
+        }
+
+        void startProcess(string fullFilePath)
+        {
+            System.Diagnostics.Process.Start(fullFilePath);
+        }
+
+        void loadBaseTree()
+        {
+            string[] localDirs = getLogicalDrives();
+            foreach (string s in localDirs)
+            {
+                TreeNode tn = _treeView.Nodes.Add(s);
+                tn.ImageIndex = 1;
+            }
+        }
+
+        string[] getDirsInDir(string dir)
+        {
+            string[] dirs = Directory.GetDirectories(dir);
+            return dirs;
+        }
+
+        private void _goButton_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void _listView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
